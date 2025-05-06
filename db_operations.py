5e77348f13cf008569d2772dde715648755c70f4
Alex Cannizzaro <110434191+abcannizz@users.noreply.github.com>
2025-05-06 16:33:19 -0700

    created filed

---
+++ db_operations.py
@@ -0,0 +1,95 @@
+from DBFinalVidyagaem.helper import helper
+
+class db_operations():
+    # constructor with connection path to DB
+    def __init__(self):
+        #make connection
+        self.connection = mysql.connector.connect(host="localhost",
+        user="root",
+        password="CPSC408!",
+        auth_plugin='mysql_native_password',
+        database="RideShare")
+        #create cursor object
+        self.cursor = self.connection.cursor()
+        
+
+    # function to simply execute a DDL or DML query.
+    # commits query, returns no results. 
+    # best used for insert/update/delete queries with no parameters
+    def modify_query(self, query):
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    # function to simply execute a DDL or DML query with parameters
+    # commits query, returns no results. 
+    # best used for insert/update/delete queries with named placeholders
+    def modify_query_params(self, query, dictionary):
+        self.cursor.execute(query, dictionary)
+        self.connection.commit()
+
+    # function to simply execute a DQL query
+    # does not commit, returns results
+    # best used for select queries with no parameters
+    def select_query(self, query):
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    # function to simply execute a DQL query with parameters
+    # does not commit, returns results
+    # best used for select queries with named placeholders
+    def select_query_params(self, query, dictionary):
+        self.cursor.execute(query, dictionary)
+        return self.cursor.fetchall()
+
+    # function to return the value of the first row's 
+    # first attribute of some select query.
+    # best used for querying a single aggregate select 
+    # query with no parameters
+    def single_record(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchone()
+        if result == None:
+            return None
+        else:
+            return result[0]
+    
+    # function to return the value of the first row's 
+    # first attribute of some select query.
+    # best used for querying a single aggregate select 
+    # query with named placeholders
+    def single_record_params(self, query, dictionary):
+        self.cursor.execute(query, dictionary)
+        result = self.cursor.fetchone()
+        if result == None:
+            return None
+        else:
+            return result[0]
+    
+    # function to return a single attribute for all records 
+    # from some table.
+    # best used for select statements with no parameters
+    def single_attribute(self, query):
+        self.cursor.execute(query)
+        results = self.cursor.fetchall()
+        results = [i[0] for i in results]
+        #results.remove(None)
+        return results
+    
+    # function to return a single attribute for all records 
+    # from some table.
+    # best used for select statements with named placeholders
+    def single_attribute_params(self, query, dictionary):
+        self.cursor.execute(query,dictionary)
+        results = self.cursor.fetchall()
+        results = [i[0] for i in results]
+        return results
+    
+    # function for bulk inserting records
+    # best used for inserting many records with parameters
+    def bulk_insert(self, query, data):
+        self.cursor.executemany(query, data)
+        self.connection.commit()
+
+    # destructor that closes connection with DB
+    def destructor(self):
+        self.cursor.close()
---
+++ videogame_app.py
@@ -0,0 +1,8 @@
+#imports
+from DBFinalVidyagaem.helper import helper
+from DBFinalVidyagaem.db_operations import db_operations
+
+db_ops = db_operations()
+
+#main method
+db_ops.destructor()
